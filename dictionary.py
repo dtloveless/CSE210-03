c27415574c8bbe946910f747c3125d86af190da0
Julie Cowley <juliecowley98@gmail.com>
2022-02-04 15:20:29 -0700

    filled out dictionary class

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,28 @@
 import random
 
+words = ['vanish','health','parsimonious','support','kettle','beef','ajar','curious','imagine','shop']
+
 class Dictionary:
     def __init__(self):
-        self.currentWord = ''
+        self._current_word = random.choice(words)
 
     def get_word(self):
-        pass
+        return self._current_word
 
-    def check_guess(self, player):
-        pass
+    def check_guess(self, playerGuess):
+        for letter in self._current_word:
+            if playerGuess == letter:
+                return True
+        return False
 
+    def display_underscores(self):
+        for x in range(len(self._current_word)):
+            print('_ ', end= '')
+        print('\n') #get an endline after the underscores
+
+test = Dictionary()
+print(f"Current Word: {test.get_word()}")
+test.display_underscores()
+
+print(test.check_guess('f'))
+print(test.check_guess('e'))

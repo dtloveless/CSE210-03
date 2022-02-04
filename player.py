d67f6041532ba55ed5ff57bfcdc3bfc643845a8c
Darcy Merilan <darcymer@byui.edu>
2022-02-04 15:11:24 -0700

    comments on what i need to due for get_guess and Past_guess

--- player.py
+++ player.py
@@ -1,11 +1,18 @@
 class player:
 
     def __init__(self):
-        self._getguess()
+        self._Get_guess()
+        self._pastguess()
     
 
-    def Get_guess(self):
+    def _Get_guess(self):
+        '''This ask the user to put in a 1 character
+        guess into the code and stores that character
+        for the dictionary to use'''
         pass
 
-    def pastguess():
+    def _Past_Guess():
+        '''This list all the previous inputs that was 
+        made by the player, and makes them unable to 
+        use them going forward in the game.'''
         pass

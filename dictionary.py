cfd5c8c1a61855a5f0294622858e72ad483b40c2
Julie Cowley <juliecowley98@gmail.com>
2022-02-04 15:00:35 -0700

    added Dictionary stub methods

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+import random
+
+class Dictionary:
+    def __init__(self):
+        self.currentWord = ''
+
+    def get_word(self):
+        pass
+
+    def check_guess(self, player):
+        pass
+

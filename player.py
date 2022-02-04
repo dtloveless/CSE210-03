458acfed3b91d96b811e34ba87bb8bef0f28bee5
Darcy Merilan <darcymer@byui.edu>
2022-02-04 15:27:41 -0700

    pain

--- player.py
+++ player.py
@@ -1,17 +1,18 @@
 class Player:
 
     def __init__(self):
-        self._get_guess()
+        self._playerGuess()
         self._past_guess()
     
 
-    def _get_guess(self,playerchoice):
+    def _playerGuess(self,playerchoice):
         '''This ask the user to put in a 1 character
         guess into the code and stores that character
         for the dictionary to use'''
         playerchoice = input("Guess a Letter [a-z]: ")
         pc = playerchoice.lower
-        pass
+        return pc
+        #needs to be limited to 1 letter
 
     def _past_guess():
         '''This list all the previous inputs that was 

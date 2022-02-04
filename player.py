cd5d932f1398e8a00a0a4a42a88e1f233474b8fa
Darcy Merilan <darcymer@byui.edu>
2022-02-04 15:13:11 -0700

    lettering

--- player.py
+++ player.py
@@ -1,17 +1,17 @@
 class player:
 
     def __init__(self):
-        self._Get_guess()
-        self._pastguess()
+        self._get_guess()
+        self._past_guess()
     
 
-    def _Get_guess(self):
+    def _get_guess(self):
         '''This ask the user to put in a 1 character
         guess into the code and stores that character
         for the dictionary to use'''
         pass
 
-    def _Past_Guess():
+    def _past_guess():
         '''This list all the previous inputs that was 
         made by the player, and makes them unable to 
         use them going forward in the game.'''
